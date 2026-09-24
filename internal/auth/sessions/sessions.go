8c1862708a6ff7334fb7533a3f7d09d24114bff1
Mike Slayback <Scott.M.Slayback@gmail.com>
2026-09-24 16:57:38 -0700

    2.4: added session expiration to cookie.

--- internal/auth/sessions/sessions.go
+++ internal/auth/sessions/sessions.go
@@ -51,9 +51,10 @@ func HasRecentAuthentication(current accounts.CurrentSession, now time.Time) boo
 
 func SetCookie(responseWriter http.ResponseWriter, session accounts.Session) {
 	http.SetCookie(responseWriter, &http.Cookie{
-		Name:  CookieName,
-		Value: session.Token,
-		Path:  "/",
+		Name:    CookieName,
+		Value:   session.Token,
+		Path:    "/",
+		Expires: session.ExpiresAt,
 	})
 }
 
